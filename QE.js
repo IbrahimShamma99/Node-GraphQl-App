0f261397504bb8d404249754f8d596defed03e15
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-25 18:34:55 +0200

    update

--- QE.js
+++ QE.js
@@ -27,3 +27,12 @@ app.use('/graphql', graphqlHTTP({
 app.listen(4000, () => {;
     console.log('Running a GraphQL API server at http://localhost:4000/graphql');
 });
+
+
+
+/**NOTE To run querries from command line
+ * curl -X POST \
+   -H "Content-Type: application/json" \
+   -d '{"query": "{ hello }"}' \
+   http://localhost:4000/graphql
+ */
---
+++ server.js
@@ -0,0 +1,41 @@
+/**
+ * In most situations, all you need to do is to 
+ * specify the types for your API using the 
+ * GraphQL schema language, taken as an argument
+ * to the buildSchema function.
+ */
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+var app = express();
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+    },
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000, () => {
+    console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
